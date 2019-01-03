aed8c11edc6a7eecc83498b998c69acc1a46c1ca
suresh81 <ssureshbbabu80mca@gmail.com>
2019-01-03 17:32:09 +0530

    Read Excel Committed...

---
+++ Read_Excel_value/src/excel/read_data_from_excel_example.java
@@ -0,0 +1,27 @@
+package excel;
+
+import java.io.File;
+import jxl.Workbook;
+
+
+public class read_data_from_excel_example {
+
+	public static void main(String[] args) throws Exception {
+	
+		File src = new File("C:/Test_files/datas.xls");
+		Workbook wb = Workbook.getWorkbook(src);
+		String data00 = wb.getSheet(0).getCell(0,0).getContents();
+		System.out.println("Data is:"+data00);
+		String data01 = wb.getSheet(0).getCell(0,1).getContents();
+		System.out.println("Data is :"+data01);
+		
+		int rows = wb.getSheet(0).getRows();
+		System.out.println("Total no of Rows:"+rows);
+		
+		int columns = wb.getSheet(0).getColumns();
+		System.out.println("Total no of Columns:"+columns);
+		
+
+	}
+
+}
