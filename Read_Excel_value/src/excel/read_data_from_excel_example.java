5cd9efb31233ee2b99d6ec701f47001c39085e34
suresh81 <ssureshbbabu80mca@gmail.com>
2019-01-17 16:42:00 +0530

    17-01-19:16:41 code updates

--- Read_Excel_value/src/excel/read_data_from_excel_example.java
+++ Read_Excel_value/src/excel/read_data_from_excel_example.java
@@ -8,6 +8,8 @@ public class read_data_from_excel_example {
 
 	public static void main(String[] args) throws Exception {
 	
+		System.out.println("Modification only done without committ message.......");
+		
 		File src = new File("C:/Test_files/datas.xls");
 		Workbook wb = Workbook.getWorkbook(src);
 		String data00 = wb.getSheet(0).getCell(0,0).getContents();
